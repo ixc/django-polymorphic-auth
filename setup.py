18c47a467f776dcf31537bfe8c531322fee17b93
Tai Lee <real.human@mrmachine.net>
2015-10-25 23:56:10 +1100

    Updates for distribution on package index.
    * Ignore dist files.
    * Build universal wheels for distribution.
    * Convert `README.md` to reStructuredText to use as long description.
    * Do not import app package.
    * Add required metadata.
    * Include all files as package data.

--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
-import setuptools
+from __future__ import print_function
 
-from polymorphic_auth import __version__
+import setuptools
+import sys
+
+version = '0.1.dev0'
+
+# Convert README.md to reStructuredText.
+if {'bdist_wheel', 'sdist'}.intersection(sys.argv):
+    try:
+        import pypandoc
+    except ImportError:
+        print('WARNING: You should install `pypandoc` to convert `README.md` '
+              'to reStructuredText to use as long description.',
+              file=sys.stderr)
+    else:
+        print('Converting `README.md` to reStructuredText to use as long '
+              'description.')
+        long_description = pypandoc.convert('README.md', 'rst')
 
 setuptools.setup(
     name='django-polymorphic-auth',
-    version=__version__,
+    version=version,
+    author='Interaction Consortium',
+    author_email='studio@interaction.net.au',
+    url='https://github.com/ixc/django-polymorphic-auth',
+    description='Polymorphic user model with plugins for common options, plus '
+                'abstract and mixin classes to create your own.',
+    long_description=locals().get('long_description', ''),
+    license='MIT',
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         'coverage',
         'Django',
